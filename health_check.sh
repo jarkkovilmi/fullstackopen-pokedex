d6e79cab6f43860cdc19512267eeffa91b294f5b
Jarkko Vilmi <jarkko.vilmi@gmail.com>
2023-02-28 16:42:23 +0200

    add custom health check script

--- health_check.sh
+++ health_check.sh
@@ -2,4 +2,12 @@
 
 echo "Hello from shell script"
 
-exit 0 # exit status 1 means that the script "fails", 0 "succeeds"
+response=$(curl "https://hidden-glitter-7604.fly.dev/health")
+if [[ $response == "ok" ]]
+then
+	echo "health_check response: $response"
+	exit 0 # exit status 0 means that the script "succeeds"
+else
+	echo "health_check script failed, response: $response"
+	exit 1 # exit status 1 means that the script "fails"
+fi
